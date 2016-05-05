c28c89369f62beb0aecbfacd7ddb92f37b45f07c
Brady Ericksen <bradyericksen@gmail.com>
2016-05-04 19:01:17 -0600

    Fleshed out function for checking CMC, including regex Added loop to keep finding random cards until one is found Added code path for if random checkbox is not checked for CMC, checkCMC is called

--- magicTCGRandomizer/Form1.cs
+++ magicTCGRandomizer/Form1.cs
@@ -11,6 +11,7 @@ using HtmlAgilityPack;
 using ScrapySharp.Extensions;
 using ScrapySharp.Network;
 using ScrapySharp.Html.Forms;
+using System.Text.RegularExpressions;
 
 namespace magicTCGRandomizer
 {
@@ -28,10 +29,31 @@ namespace magicTCGRandomizer
                 //Browser.AutoDownloadPagesResources = true;
                 //Browser.AllowAutoRedirect = true; // Browser has settings you can access in setup
                 //Browser.AllowMetaRedirect = true;
-                var getHtmlWeb = new HtmlWeb();
                 //var document = getHtmlWeb.Load("http://gatherer.wizards.com/Pages/Card/Details.aspx?action=random");
-                //http://gatherer.wizards.com/Pages/Card/Details.aspx?multiverseid=132069
-                var document = getHtmlWeb.Load("http://gatherer.wizards.com/Pages/Card/Details.aspx?multiverseid=132069");
+                //WebPage home = Browser.NavigateToPage(new Uri("http://www.ietf.org"));
+                //WebPage PageResult = Browser.NavigateToPage(new Uri("http://gatherer.wizards.com/Pages/Default.aspx"));
+                //WebPage PageResult = Browser.NavigateToPage(new Uri("http://gatherer.wizards.com/Pages/Advanced.aspx"));
+                //webBrowser1.Navigate(new Uri("http://gatherer.wizards.com/Pages/Card/Details.aspx?action=random"));
+                //WebPage PageResult = Browser.NavigateToPage(new Uri("http://gatherer.wizards.com/Pages/Card/Details.aspx?action=random"));
+                //HtmlNode ColorNode = PageResult.Html.CssSelect("#ctl00_ctl00_ctl00_MainContent_SubContent_SubContent_manaRow > div:nth-child(2) > img:nth-child(1)").First();
+                //HtmlNode TitleNode = PageResult.Html.CssSelect("#ctl00_ctl00_ctl00_MainContent_SubContent_SubContent_cardImage").First();
+
+                bool cardFound = false;
+
+                var getHtmlWeb = new HtmlWeb(); //create new HtmlWeb object
+                while(cardFound == false) //loop until card meeting criteria is matched
+                {
+                    var document = getHtmlWeb.Load("http://gatherer.wizards.com/Pages/Card/Details.aspx?multiverseid=132069"); //load a specific page for now. Will eventually be the random card page on gatherer
+                    if (checkBoxCMCRandom.Checked != true) //if RandomCMC checkbox is not checked
+                    {
+                        if (checkCMC(document, numericUpDownCMC.Value.ToString()) == false) //check cards CMC against specified CMC
+                        {
+                            continue; //CMC didn't match, keep looping
+                        }
+                    }
+                }
+
+
                 /*var nodes = document.DocumentNode.SelectNodes("//div[@id='ctl00_ctl00_ctl00_MainContent_SubContent_SubContent_cmcRow']");
                 if (nodes != null)
                 {
@@ -40,29 +62,35 @@ namespace magicTCGRandomizer
                         MessageBox.Show(node.InnerText);
                     }
                 }*/
-                var CMCNode = document.GetElementbyId("ctl00_ctl00_ctl00_MainContent_SubContent_SubContent_cmcRow");
-                if(CMCNode != null)
-                {
-                    string temp = CMCNode.InnerText;
-                    //now use regular expressions to strip string and get important info
-                }
-
-                //WebPage home = Browser.NavigateToPage(new Uri("http://www.ietf.org"));
-                //WebPage PageResult = Browser.NavigateToPage(new Uri("http://gatherer.wizards.com/Pages/Default.aspx"));
-                //WebPage PageResult = Browser.NavigateToPage(new Uri("http://gatherer.wizards.com/Pages/Advanced.aspx"));
-                //http://gatherer.wizards.com/Pages/Card/Details.aspx?action=random
-                //webBrowser1.Navigate(new Uri("http://gatherer.wizards.com/Pages/Card/Details.aspx?action=random"));
-                //WebPage PageResult = Browser.NavigateToPage(new Uri("http://gatherer.wizards.com/Pages/Card/Details.aspx?action=random"));
-                //PageWebForm form = PageResult.FindFormById("dataForm");
-                //HtmlNode ColorNode = PageResult.Html.CssSelect("#ctl00_ctl00_ctl00_MainContent_SubContent_SubContent_manaRow > div:nth-child(2) > img:nth-child(1)").First();
-                //string temp = ColorNode.InnerText;
-                //HtmlNode TitleNode = PageResult.Html.CssSelect("#ctl00_ctl00_ctl00_MainContent_SubContent_SubContent_cardImage").First();
-                //TitleNode.
             }
             catch(Exception exp)
             {
                 Console.WriteLine(exp);
             }
         }
+
+        public bool checkCMC(HtmlAgilityPack.HtmlDocument document, string cmc)
+        {
+            string result = "";
+            var CMCNode = document.GetElementbyId("ctl00_ctl00_ctl00_MainContent_SubContent_SubContent_cmcRow"); //find the CMC element via ID
+            if (CMCNode != null)
+            {
+                string temp = CMCNode.InnerText;
+                result = Regex.Match(temp, "\\d").ToString(); //regex match to find only the digit //check to make sure double digits work correctly
+            }
+            else
+            {
+                Console.WriteLine("Something went wrong. We didn't find the correct CMC id on the page.");
+                return false;
+            }
+            if(result.Equals(cmc))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        } 
     }
 }
